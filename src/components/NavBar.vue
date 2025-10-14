186760a7f91438e6cbfba186b8b30c899f60ba36
Halid Kartal <halidkartal09@gmail.com>
2025-10-14 09:41:29 +0200

    a
    a

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -119,8 +119,8 @@
     top: 0;
     left: 0;
     width: 105vw;
-    height: 100vh; /* zauzima cijeli ekran */
-    background: rgba(0, 0, 0, 0.98); /* crna pozadina */
+    height: 100vh; 
+    background: rgba(0, 0, 0, 0.99); 
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -201,7 +201,7 @@
     position: fixed;
     top: 0;
     left: 10;
-    padding: 20px;
+    padding: 0px;
     z-index: 1001;
   }
 
