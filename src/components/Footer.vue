f3de5e8c6aeadaad463973e9e0eb1a35295fc199
Halid Kartal <halidkartal09@gmail.com>
2025-10-14 10:55:04 +0200

    a
    a

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -98,8 +98,8 @@
   clip-path: polygon(0 20%, 145% 0, 127% 100%, 0% 100%);
   }
   .footerTitleText {
-    font-size: 2.8em;
-    padding: 4px 8px;
+    font-size: 3.2em;
+    padding: 14px 18px;
   }
 
   .emailTextDesign {
@@ -158,7 +158,7 @@
       <!-- CRAFTED BY DEVELOPER SIGNATURE -->
      <div class="craftedFromDeveloper">
   <span>Created by me</span><br />
-  <svg xmlns='@/assets/halidkartal.png' viewBox="0 0 600 120" width="180" height="50">
+  <svg xmlns='@/assets/halidkartal.png' viewBox="0 0 600 120" width="140" height="50">
     <text x="10" y="130" 
           font-family="Brush Script MT, cursive" 
           font-size="130" 
