1b1debff426a8162cd41389d575d028fc3417374
Halid Kartal <halidkartal09@gmail.com>
2025-10-14 12:57:11 +0200

    a
    a

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -47,7 +47,6 @@
 .craftedFromDeveloper {
   text-align: center;
   margin-top: 20px;
-  
 }
 
 .signature-text {
@@ -129,7 +128,7 @@
 
   .footerTitleText {
     font-size: 2.2em;
-    padding: 15px 25px;
+    padding: 15px 15px;
   }
 
   .emailTextDesign {
@@ -170,7 +169,7 @@
      <div class="craftedFromDeveloper">
   <span>Created by me</span><br />
   <svg viewBox="0 0 640 130" width="200" height="60" preserveAspectRatio="xMinYMin meet">
-    <text x="-2" y="110" class="signature-text">
+    <text x="-1" y="110" class="signature-text">
       Halid Kartal
     </text>
   </svg>
